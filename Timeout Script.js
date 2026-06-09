22daf8f73a85ed268e182c04e7d9578568d0e3a6
itszprimeclipzz-cpu <itszprimeclipzz@gmail.com>
2026-06-09 15:03:14 -0500

    Change protected user ID in timeout script

--- Timeout Script.js
+++ Timeout Script.js
@@ -1,4 +1,4 @@
-if (message.mentions.users.has('USER_ID_TO_PROTECT')) {
+if (message.mentions.users.has('1388187970493743125')) {
     if (!message.member.permissions.has('ModerateMembers')) { // don't timeout mods
         message.member.timeout(10 * 60 * 1000, 'Pinged protected user') // 10 min
     }

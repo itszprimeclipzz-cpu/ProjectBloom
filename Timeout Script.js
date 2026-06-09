8acb1aeea4fe8ffcea40a840beee937e84710648
itszprimeclipzz-cpu <itszprimeclipzz@gmail.com>
2026-06-09 15:01:49 -0500

    Update Timeout Script.js

--- Timeout Script.js
+++ Timeout Script.js
@@ -1,1 +1,5 @@
-
+if (message.mentions.users.has('USER_ID_TO_PROTECT')) {
+    if (!message.member.permissions.has('ModerateMembers')) { // don't timeout mods
+        message.member.timeout(10 * 60 * 1000, 'Pinged protected user') // 10 min
+    }
+}
